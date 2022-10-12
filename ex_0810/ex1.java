a0b5b0b2a50803ac74f7505b1adb53a811ed7077
Hai <tranhoanghai126@gmail.com>
2022-10-12 20:43:40 +0700

    Exercise 8/10

---
+++ ex_0810/ex1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so nguyen duong n: ");
+        int n = sc.nextInt();
+        int total = 0;
+        for(int i=0; i<=n; i++){
+            if(i%2==0){
+                total+=i;
+            }
+        }
+        System.out.println("Tong cac so chan la: " + total);
+    }
+}
