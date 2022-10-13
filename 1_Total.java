37e89bd3476d91738c788907e838e209a3f60356
HoangAnh <anhitachi17@gmail.com>
2022-10-14 00:47:45 +0700

    format code and change variable

--- 1_Total.java
+++ 1_Total.java
@@ -4,9 +4,9 @@ public class Total {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Nhap so n:  ");
-		int n = sc.nextInt();
+		int number = sc.nextInt();
 		int total = 0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < number; i++) {
 			if (i % 2 == 0) {
 				total += i;
 			}
